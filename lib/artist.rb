112ca6948bce3cc26dc3ff26cc6003df65e19f0d
Alan Young <alanryoung01@gmail.com>
2020-10-01 15:56:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name, :songs
 
-  @@counter = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -10,18 +10,18 @@ class Artist
 
   def add_song(song)
     song.artist = self
-    @@counter += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@counter += 1
+    @@song_count += 1
   end
 
   def self.counter
-    @@counter
+    @@song_count
   end
 
 end
