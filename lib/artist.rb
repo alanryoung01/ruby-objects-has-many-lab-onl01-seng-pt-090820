ab8d6e71f74f104e914ee58aac2e2405f5d61004
Alan Young <alanryoung01@gmail.com>
2020-10-01 16:22:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,35 +1,25 @@
 class Artist
-  attr_accessor :name, :songs
-
-  @@song_count = 0
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @songs = []
+
   end
 
-def songs
-  Song.all.select do |song|
-    song.artist == self
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
-end
 
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    add_song(song)
   end
 
   def self.song_count
-    @@song_count += 1
-    @@song_count
+    Song.all.count
   end
-
 end
