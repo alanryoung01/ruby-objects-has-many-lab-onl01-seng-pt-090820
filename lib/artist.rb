f70c8b412e77a3f6f0d613450e515cd3e96332a7
Alan Young <alanryoung01@gmail.com>
2020-10-01 16:20:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,12 @@ class Artist
     @songs = []
   end
 
+def songs
+  Song.all.select do |song|
+    song.artist == self
+  end
+end
+
   def add_song(song)
     @songs << song
     song.artist = self
